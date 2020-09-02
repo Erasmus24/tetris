74da235514f36906edc8e11b6a2eccddb65fd95c
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-09-02 02:33:39 +0200

    installation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is the Tetris game app!
+      </header>
+    </div>
+  );
+}
+
+export default App;
